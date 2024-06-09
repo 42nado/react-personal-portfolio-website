5795e44a94ee209157fc33541a2138a217ca5b64
Ironsick_ <daryllfortunado@gmail.com>
2024-06-09 09:03:14 +0800

    refactor: Update Contact form component to improve email sending functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,32 +4,32 @@ import emailjs from '@emailjs/browser';
 import Swal from 'sweetalert2';
 
 const Contact = () => {
-    const form = useRef();
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_1orcuvn', 'template_hwemm2z', form.current, 'nufWVpy63w_2mRZyk')
       .then((result) => {
-          console.log("Email sent successfully");
-          console.log(result.text);
-          // alert("Email sent successfully");
-          Swal.fire(
-            'Email Sent Successfully!',
-            '',
-            'success'
-          )
+        console.log("Email sent successfully");
+        console.log(result.text);
+        // alert("Email sent successfully");
+        Swal.fire(
+          'Email Sent Successfully!',
+          '',
+          'success'
+        )
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   };
   return (
-    <form ref={form} onSubmit={sendEmail}>     
-        <input name="subject" type="text" class="feedback-input" placeholder="Subject" required/>  
-        <input name="name" type="text" class="feedback-input" placeholder="Name" required/>   
-        <input name="email" type="email" class="feedback-input" placeholder="Email"  required/>
-        <textarea name="message" class="feedback-input" placeholder="Comment" required ></textarea>
-        <input type="submit" value="Send"/>
+    <form ref={form} onSubmit={sendEmail}>
+      <input name="subject" type="text" class="feedback-input" placeholder="Subject" required />
+      <input name="name" type="text" class="feedback-input" placeholder="Name" required />
+      <input name="email" type="email" class="feedback-input" placeholder="Email" required />
+      <textarea name="message" class="feedback-input" placeholder="Message" required ></textarea>
+      <input type="submit" value="Send" />
     </form>
   )
 }
